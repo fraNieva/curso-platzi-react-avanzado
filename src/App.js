ed4c019a3221ce5ee9e8b6678b2a29991afe0d4f
Franco Nieva <franconieva86@gmail.com>
2019-10-23 14:53:58 -0300

    Navbar component implementation

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { GlobalStyle } from './styles/GlobalStyle'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
 import { Logo } from './components/Logo'
+import { Navbar } from './components/Navbar'
 import { AppWrapper } from './styles'
 
 export const App = () => {
@@ -16,6 +17,7 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <Navbar />
     </AppWrapper>
   )
 }
