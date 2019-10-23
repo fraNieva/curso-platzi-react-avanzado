be1d2ef99a8be4c6e1f200d7abd70fb7b871377e
Franco Nieva <franconieva86@gmail.com>
2019-10-23 15:50:41 -0300

    Auth in favs and user pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ import { Router } from '@reach/router'
 import { GlobalStyle } from './styles/GlobalStyle'
 import { Home } from './pages/Home'
 import { Detail } from './pages/Detail'
+import { User } from './pages/User'
+import { Favs } from './pages/Favs'
+import { NotRegisteredUser } from './pages/NotRegisteredUser'
 import { Logo } from './components/Logo'
 import { Navbar } from './components/Navbar'
 import { AppWrapper } from './styles'
+
+const UserLogged = ({ children }) => {
+  return children({ isAuth: true })
+}
 
 export const App = () => {
   return (
@@ -17,6 +24,23 @@ export const App = () => {
         <Home path='/pet/:id' />
         <Detail path='/detail/:detailId' />
       </Router>
+      <UserLogged>
+        {
+          ({ isAuth }) =>
+            isAuth
+              ? (
+                <Router>
+                  <Favs path='/favs' />
+                  <User path='/user' />
+                </Router>
+              ) : (
+                <Router>
+                  <NotRegisteredUser path='/favs' />
+                  <NotRegisteredUser path='/user' />
+                </Router>
+              )
+        }
+      </UserLogged>
       <Navbar />
     </AppWrapper>
   )
